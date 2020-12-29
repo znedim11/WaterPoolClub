cf0dd20b2ba8c6650235ce86fd2381420afa2383
Edin Purivatra <edin.purivatra@edu.fit.ba>
2020-12-29 12:53:39 +0100

    finished sprint3

--- VaterpoloKlub/Controllers/RezultatTestiranjaController.cs
+++ VaterpoloKlub/Controllers/RezultatTestiranjaController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using VaterpoloKlub.Data;
 using VaterpoloKlub.Models;
+using VaterpoloKlub.ViewModels;
 
 namespace VaterpoloKlub.Controllers
 {
@@ -19,12 +20,7 @@ namespace VaterpoloKlub.Controllers
             _context = context;
         }
 
-        // GET: RezultatTestiranja
-        public async Task<IActionResult> Index()
-        {
-            var applicationDbContext = _context.RezultatiTestiranja.Include(r => r.Clan).Include(r => r.VjezbeTestiranje);
-            return View(await applicationDbContext.ToListAsync());
-        }
+        
 
         // GET: RezultatTestiranja/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -34,44 +30,28 @@ namespace VaterpoloKlub.Controllers
                 return NotFound();
             }
 
-            var rezultatTestiranja = await _context.RezultatiTestiranja
-                .Include(r => r.Clan)
-                .Include(r => r.VjezbeTestiranje)
-                .FirstOrDefaultAsync(m => m.ClanId == id);
+            var rezultatTestiranja = await _context.Clanovi.Include(x => x.RezultatiTestiranja).ToListAsync();
+            List<RezultatTestiranja> temp = new List<RezultatTestiranja>();
+            foreach (var clan in rezultatTestiranja)
+            {
+                clan.RezultatiTestiranja = clan.RezultatiTestiranja.Where(x => x.TestiranjeId == id).ToList();
+            }
             if (rezultatTestiranja == null)
             {
                 return NotFound();
             }
-
-            return View(rezultatTestiranja);
-        }
-
-        // GET: RezultatTestiranja/Create
-        public IActionResult Create()
-        {
-            ViewData["ClanId"] = new SelectList(_context.Clanovi, "ID", "ID");
-            ViewData["VjezbaId"] = new SelectList(_context.VjezbeTestiranje, "TestirajeId", "TestirajeId");
-            return View();
-        }
-
-        // POST: RezultatTestiranja/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TestiranjeId,VjezbaId,ClanId,Rezultat")] RezultatTestiranja rezultatTestiranja)
-        {
-            if (ModelState.IsValid)
+            var vjezbeTestiranje = await _context.VjezbeTestiranje.Include(x => x.Vjezba).Where(x => x.TestiranjeId == id).ToListAsync();
+            var nesto = new RezultatTestiranjaViewModel
             {
-                _context.Add(rezultatTestiranja);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ClanId"] = new SelectList(_context.Clanovi, "ID", "ID", rezultatTestiranja.ClanId);
-            ViewData["VjezbaId"] = new SelectList(_context.VjezbeTestiranje, "TestirajeId", "TestirajeId", rezultatTestiranja.VjezbaId);
-            return View(rezultatTestiranja);
+                Clanovi = rezultatTestiranja,
+                VjezbeTestiranje = vjezbeTestiranje
+            };
+            //ViewData["rezTestiranja"] = rezultatTestiranja;
+            //ViewData["vjTestiranja"] = vjezbeTestiranje;
+            return View(nesto);
         }
 
+      
         // GET: RezultatTestiranja/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -91,9 +71,14 @@ namespace VaterpoloKlub.Controllers
                 return NotFound();
             }
             var vjezbeTestiranje = await _context.VjezbeTestiranje.Include(x => x.Vjezba).Where(x => x.TestiranjeId == id).ToListAsync();
-            ViewData["rezTestiranja"] = rezultatTestiranja;
-            ViewData["vjTestiranja"] = vjezbeTestiranje;
-            return View();
+            var nesto = new RezultatTestiranjaViewModel
+            {
+                Clanovi = rezultatTestiranja,
+                VjezbeTestiranje = vjezbeTestiranje
+            };
+            //ViewData["rezTestiranja"] = rezultatTestiranja;
+            //ViewData["vjTestiranja"] = vjezbeTestiranje;
+            return View(nesto);
         }
 
         // POST: RezultatTestiranja/Edit/5
@@ -101,69 +86,16 @@ namespace VaterpoloKlub.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("TestiranjeId,VjezbaId,ClanId,Rezultat")] RezultatTestiranja rezultatTestiranja)
+        public async Task<IActionResult> Edit(int id, RezultatTestiranjaViewModel nesto)
         {
-            if (id != rezultatTestiranja.ClanId)
+            foreach (var item in nesto.Clanovi)
             {
-                return NotFound();
+                _context.Update(item);
             }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(rezultatTestiranja);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!RezultatTestiranjaExists(rezultatTestiranja.ClanId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ClanId"] = new SelectList(_context.Clanovi, "ID", "ID", rezultatTestiranja.ClanId);
-            ViewData["VjezbaId"] = new SelectList(_context.VjezbeTestiranje, "TestirajeId", "TestirajeId", rezultatTestiranja.VjezbaId);
-            return View(rezultatTestiranja);
-        }
-
-        // GET: RezultatTestiranja/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var rezultatTestiranja = await _context.RezultatiTestiranja
-                .Include(r => r.Clan)
-                .Include(r => r.VjezbeTestiranje)
-                .FirstOrDefaultAsync(m => m.ClanId == id);
-            if (rezultatTestiranja == null)
-            {
-                return NotFound();
-            }
-
-            return View(rezultatTestiranja);
-        }
-
-        // POST: RezultatTestiranja/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var rezultatTestiranja = await _context.RezultatiTestiranja.FindAsync(id);
-            _context.RezultatiTestiranja.Remove(rezultatTestiranja);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return Redirect("/Testiranje/Index");
         }
-
+                
         private bool RezultatTestiranjaExists(int id)
         {
             return _context.RezultatiTestiranja.Any(e => e.ClanId == id);
